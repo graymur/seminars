185c97156e4c16ce6b395b92aaeda894aaab6be7
graymur <graymur@mail.ru>
2016-08-04 11:23:03 +0300

    removed default state

--- 04_react-redux/src/js/redux/store.js
+++ 04_react-redux/src/js/redux/store.js
@@ -1,13 +1,4 @@
 import { createStore } from 'redux';
 import reducer from './reducer.js';
 
-const store = createStore(reducer, {
-    counter: 0,
-    todos: [
-        { id: 1, "title": "Выучить Реакт", "completed": true },
-        { id: 2, "title": "Выучить Редукс", "completed": false },
-        { id: 3, "title": "Выучить Вебпак", "completed": false }
-    ]
-});
-
-export default store;
+export default createStore(reducer);
